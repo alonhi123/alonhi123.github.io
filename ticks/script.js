065cb596112d43305b216f1403a78dffeebc6a24
alonhi <96878652+alonhi123@users.noreply.github.com>
2024-05-15 23:30:49 +0300

    Delete ticks directory

--- ticks/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("ticks-calculator").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const seconds = parseInt(document.getElementById("seconds").value);
-    const ticks = seconds * 20; // Каждая секунда в Minecraft равна 20 тикам
-    document.getElementById("result").innerText = `Количество тиков: ${ticks}`;
-});
