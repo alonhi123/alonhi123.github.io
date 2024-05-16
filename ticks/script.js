09d4ab2ae8c9f87c1620780e221da6d1f97da7a2
alonhi <karsonqqq@gmail.com>
2024-05-16 15:57:49 +0900

    Mega update ticks minecraft

--- ticks/script.js
+++ ticks/script.js
@@ -1,6 +1,13 @@
 document.getElementById("ticks-calculator").addEventListener("submit", function(event) {
     event.preventDefault();
+  
+    const days = parseInt(document.getElementById("days").value);
+    const hours = parseInt(document.getElementById("hours").value);
+    const minutes = parseInt(document.getElementById("minutes").value);
     const seconds = parseInt(document.getElementById("seconds").value);
-    const ticks = seconds * 20; // Каждая секунда в Minecraft равна 20 тикам
+  
+    const totalSeconds = days * 86400 + hours * 3600 + minutes * 60 + seconds;
+    const ticks = totalSeconds * 20;
+  
     document.getElementById("result").innerText = `Количество тиков: ${ticks}`;
-});
+  });
